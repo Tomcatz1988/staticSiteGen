263d2626acd90613b1c45e1b748ecf46665d6ac7
John Simm <tomcatz31588@gmail.com>
2026-03-19 18:36:18 -0400

    fixed bugs, textnodes print properly

--- src/main.py
+++ src/main.py
@@ -2,10 +2,13 @@ from textnode import *
 
 
 def main():
-    plain = TextNode("This is plain text", PLAIN_TEXT)
-    bold = TextNode("Make this bold", BOLD_TEXT)
-    italic = TextNode("This is italics", ITALIC_TEXT)
-    code = TextNode("This should be code", CODE_TEXT)
-    link = TextNode("Here is a link", LINK, "http://www.google.com")
-    image = TextNode("Look at this picture", IMAGE, "http://www.imgur.com")
-    print(f"{plain}'\n'{bold}'\n'{italic}'\n'{code}'\n'{link}'\n'{image}")
+    plain = TextNode("This is plain text", TextType.PLAIN_TEXT)
+    bold = TextNode("Make this bold", TextType.BOLD_TEXT)
+    italic = TextNode("This is italics", TextType.ITALIC_TEXT)
+    code = TextNode("This should be code", TextType.CODE_TEXT)
+    link = TextNode("Here is a link", TextType.LINK, "http://www.google.com")
+    image = TextNode("Look at this picture", TextType.IMAGE, "http://www.imgur.com")
+    print(f"{plain}\n{bold}\n{italic}\n{code}\n{link}\n{image}")
+
+if __name__ == "__main__":
+    main()
